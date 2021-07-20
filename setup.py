470e3c8b9fc9befcea494c3530decfbe2757caec
John Preston <john@ews-network.net>
2021-07-20 17:08:31 +0100

    Bump to 0.3.0

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/compose-x/ecs-files-composer",
-    version="0.2.0",
+    version="0.3.0",
     zip_safe=False,
     license="MPL-2.0",
 )
