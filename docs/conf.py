01a32ad9819deef0255154618abff2d7eb8b735e
John "Preston" Mille <john@ews-network.net>
2023-08-07 20:59:38 +0100

    Updated deps & using pre-commit action

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 import ecs_files_composer
 
@@ -32,22 +32,22 @@ import ecs_files_composer
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'ECS Files Composer'
+project = "ECS Files Composer"
 copyright = "2021, John Preston"
 author = "John Preston"
 
@@ -70,10 +70,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -137,14 +137,16 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_show_sourcelink = True
-html_sidebars = {"**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]}
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ecs_files_composerdoc'
+htmlhelp_basename = "ecs_files_composerdoc"
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -168,14 +170,22 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ecs_files_composer.tex', 'ECS Files Composer Documentation', 'John Preston', 'manual'),
+    (
+        master_doc,
+        "ecs_files_composer.tex",
+        "ECS Files Composer Documentation",
+        "John Preston",
+        "manual",
+    ),
 ]
 
 # -- Options for manual page output ------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, 'ecs_files_composer', 'ECS Files Composer Documentation', [author], 1)]
+man_pages = [
+    (master_doc, "ecs_files_composer", "ECS Files Composer Documentation", [author], 1)
+]
 
 # -- Options for Texinfo output ----------------------------------------
 
@@ -185,11 +195,11 @@ man_pages = [(master_doc, 'ecs_files_composer', 'ECS Files Composer Documentatio
 texinfo_documents = [
     (
         master_doc,
-        'ecs_files_composer',
-        'ECS Files Composer Documentation',
+        "ecs_files_composer",
+        "ECS Files Composer Documentation",
         author,
-        'ecs_files_composer',
-        'One line description of project.',
-        'Miscellaneous',
+        "ecs_files_composer",
+        "One line description of project.",
+        "Miscellaneous",
     ),
 ]
